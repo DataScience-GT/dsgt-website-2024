5dcfb9340d1bf17c2b72443367c7176ea85be80a
JohnRamberger <jramberger3@gatech.edu>
2022-10-26 23:48:17 -0400

    Update Sponsors.js

--- client/src/assets/Content/Sponsors.js
+++ client/src/assets/Content/Sponsors.js
@@ -1,7 +1,10 @@
+//load all images
+const intel = require("../images/sponsors/intel.png");
+
 export const Sponsors = [
     {
-        name: "Sponsor 1",
-        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        name: "Intel",
+        logo: intel,
         link: "https://www.google.com",
     },
     {
