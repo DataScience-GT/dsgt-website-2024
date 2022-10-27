4006cf81db13cd9cd794661686de2ce0240f94a7
JohnRamberger <jramberger3@gatech.edu>
2022-10-27 16:14:40 -0400

    Updated Sponsors Content
    added more sponsors to list

--- client/src/assets/Content/Sponsors.js
+++ client/src/assets/Content/Sponsors.js
@@ -1,16 +1,89 @@
-//load all images
+//load all images from ../images/sponsors
 const intel = require("../images/sponsors/intel.png");
-const lucid = require("../images/sponsors/lucid.png");
+const lucid = require("../images/sponsors/lucid2.png");
+const accenture = require("../images/sponsors/accenture-min.png");
+const bac = require("../images/sponsors/bac-min.png");
+const capitalone = require("../images/sponsors/capitalone-min.png");
+const citadel = require("../images/sponsors/citadel-min.png");
+const createx = require("../images/sponsors/createx-min.png");
+const deepnote = require("../images/sponsors/deepnote-min.png");
+const gatherly = require("../images/sponsors/gatherly2.png");
+const gc = require("../images/sponsors/gc-min.png");
+const ironmountain = require("../images/sponsors/ironmountain-min.png");
+const wolfram = require("../images/sponsors/wolfram-min.png");
+const statefarm = require("../images/sponsors/statefarm.png");
+const captech = require("../images/sponsors/captech.png");
 
 export const Sponsors = [
+    {
+        name: "Accenture",
+        logo: accenture,
+        link: "https://www.accenture.com/",
+    },
+    {
+        name: "GT Business Analytics Center",
+        logo: bac,
+        link: "https://www.scheller.gatech.edu/centers-initiatives/business-analytics-center/index.html",
+    },
+    {
+        name: "Capital One",
+        logo: capitalone,
+        link: "https://www.capitalone.com/",
+    },
+    {
+        name: "CapTech Consulting",
+        logo: captech,
+        link: "https://captechconsulting.com/",
+    },
+    {
+        name: "Citadel",
+        logo: citadel,
+        link: "https://www.citadel.com/",
+    },
+    {
+        name: "Create X",
+        logo: createx,
+        link: "https://create-x.gatech.edu/",
+    },
+    {
+        name: "Deepnote",
+        logo: deepnote,
+        link: "https://deepnote.com/",
+    },
+    {
+        name: "Gatherly",
+        logo: gatherly,
+        link: "https://www.gatherly.io/",
+    },
+    {
+        name: "Google Cloud",
+        logo: gc,
+        link: "https://cloud.google.com/",
+    },
     {
         name: "Intel",
         logo: intel,
         link: "https://www.intel.com",
     },
+
+    {
+        name: "Iron Mountain",
+        logo: ironmountain,
+        link: "https://www.ironmountain.com/",
+    },
     {
         name: "Lucid",
         logo: lucid,
         link: "https://lucid.co/",
+    },
+    {
+        name: "State Farm",
+        logo: statefarm,
+        link: "https://www.statefarm.com/",
+    },
+    {
+        name: "Wolfram",
+        logo: wolfram,
+        link: "https://www.wolfram.com/language/",
     },
 ];
