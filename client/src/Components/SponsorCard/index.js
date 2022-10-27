a419305056a0932b992ccb9f085a7622e99d0fdc
JohnRamberger <jramberger3@gatech.edu>
2022-10-26 23:48:30 -0400

    Updated SponsorCard

--- client/src/Components/SponsorCard/index.js
+++ client/src/Components/SponsorCard/index.js
@@ -3,10 +3,12 @@ import styles from "./SponsorCard.module.scss";
 
 const SponsorCard = (params) => {
     return (
-        <div className={styles.SponsorCard}>
-            <div className={styles.imageContainer}>
-                <img src={params.image} alt={params.name} />
-            </div>
+        <div className={styles.SponsorCard} title={params.name}>
+            <a href={params.link} target="_blank" rel="noreferrer">
+                <div className={styles.imageContainer}>
+                    <img src={params.logo} alt={params.name} />
+                </div>
+            </a>
         </div>
     );
 };
