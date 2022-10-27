e618a8bfe1caf078d87d7b8037bd5c450242a6e7
JohnRamberger <jramberger3@gatech.edu>
2022-10-26 23:30:14 -0400

    Moved SponsorContent

--- client/Content/Sponsors.js
+++
@@ -1,7 +0,0 @@
-const Sponsors = [
-    {
-        name: "Sponsor 1",
-        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-        link: "https://www.google.com",
-    },
-];
---
+++ client/src/assets/Content/Sponsors.js
@@ -0,0 +1,212 @@
+export const Sponsors = [
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+    {
+        name: "Sponsor 1",
+        logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        link: "https://www.google.com",
+    },
+];
