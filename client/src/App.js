d35d77fdc724c5cd0605873176c4af287bb43b27
JohnRamberger <jramberger3@gatech.edu>
2023-04-11 11:59:12 -0400

    fix bug with router

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,28 @@
 import styles from "./App.module.scss";
 
 //import react stuff
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 //import pages
 import Home from "./Pages/Home";
 import About from "./Pages/About";
-import { useLocation } from "react-router";
+import { useLocation } from "react-router-dom";
 import trackPathForAnalytics from "./TrackPageForAnalytics";
 import { useEffect } from "react";
 
 function App() {
-    const {pathname, search} = useLocation();
-    
+    const { pathname, search } = useLocation();
 
     useEffect(() => {
-        const analytics = () => { 
-            trackPathForAnalytics({path: pathname, search: search, title: pathname.split("/")[1]})
+        const analytics = () => {
+            trackPathForAnalytics({
+                path: pathname,
+                search: search,
+                title:
+                    pathname != null && pathname !== ""
+                        ? pathname.split("/")[1] ?? ""
+                        : "",
+            });
         };
 
         analytics();
@@ -25,12 +31,12 @@ function App() {
 
     return (
         <div className={styles.App}>
-            <Router>
-                <Routes>
-                    <Route path="/*" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                </Routes>
-            </Router>
+            {/* <Router className={styles.App}> */}
+            <Routes>
+                <Route path="/*" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+            {/* </Router> */}
         </div>
     );
 }
